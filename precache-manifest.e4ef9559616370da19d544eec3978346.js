5ed252cc489924593fe1df7c509e43e3efde2020
Santos Castillo <scastillo@grouponlatam.com>
2019-08-19 05:13:46 -0400

    Updates

---
+++ precache-manifest.e4ef9559616370da19d544eec3978346.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9a2d59b6e60cb0a1a880ee146b62bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0afa50c113adb2b38a6a",
+    "url": "/static/css/main.86cab70e.chunk.css"
+  },
+  {
+    "revision": "76a7b27b72e5707e4ee9",
+    "url": "/static/js/2.fcc20cf4.chunk.js"
+  },
+  {
+    "revision": "0afa50c113adb2b38a6a",
+    "url": "/static/js/main.6acec7b9.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  }
+]);
