99ebc2aeb767ca780f91245e2226eae3e0483628
Santos Castillo <scastillo@grouponlatam.com>
2019-08-19 05:55:14 -0400

    Updates

---
+++ precache-manifest.6da2ce86d1a27ae0ed0022af37e200a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8179d0ba04d424af26b203fbb223a899",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c23f9b2b056ca56e4354",
+    "url": "/portfolio/static/css/main.86cab70e.chunk.css"
+  },
+  {
+    "revision": "76a7b27b72e5707e4ee9",
+    "url": "/portfolio/static/js/2.fcc20cf4.chunk.js"
+  },
+  {
+    "revision": "c23f9b2b056ca56e4354",
+    "url": "/portfolio/static/js/main.0db32976.chunk.js"
+  },
+  {
+    "revision": "5029282666060c428c88",
+    "url": "/portfolio/static/js/runtime~main.cbff171f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/santosdummont.github.io/precache-manifest.25245116b81f4544038eada43233be80.js"
+  "/portfolio/precache-manifest.6da2ce86d1a27ae0ed0022af37e200a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/santosdummont.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
